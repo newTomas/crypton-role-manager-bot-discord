0cc88052dbd9d13cb37eff12c86167de1bc6e73b
mrkeksz <mrkeksz@yandex.ru>
2021-08-13 09:32:21 +0300

    Remove test

--- test/helpers/utils.ts
+++
@@ -1,9 +0,0 @@
-import {assert} from 'chai'
-
-export async function shouldThrowFunction(func: () => any, message?: string): Promise<void> {
-  try {
-    await func()
-    assert(true)
-  } catch (err) {return}
-  assert(false, message ?? 'Функция не выдала ошибку.')
-}
